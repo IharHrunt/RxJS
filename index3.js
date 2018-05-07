3271c771918cae178a5289dbc07ab9009ae4c321
Ihar Hrunt <Ihar_Hrunt@epam.com>
2018-05-07 17:36:39 +0300

    Added small changes

--- index3.js
+++ index3.js
@@ -1,7 +1,6 @@
-const { of, range, from, fromEvent, fromPromise, interval, timer, merge } = rxjs;
-const { map, filter, buffer, skip, take, skipUntil, takeUntil, 
+const { Observable, of, range, from, fromEvent, fromPromise, interval, timer, merge, zip, combineLatest} = rxjs;
+const { map, filter, buffer, skip, take, skipUntil, takeUntil, delay,
         skipWhile, takeWhile, distinctUntilChanged, mergeAll, concatAll, concatMap } = rxjs.operators;
-
 
 const s1 = interval(900).pipe(map(x => 'Stream 1: ' + x));
 const s2 = interval(500).pipe(map(x => 'Stream 2: ' + x));
@@ -10,7 +9,7 @@ merge(s1, s2)
     .pipe(take(8))
     .subscribe(subscription('merge'));
                 
-range(1, 2) // 1, 2, 3
+range(1, 2)
     .pipe(map(x => range(1, 3)), mergeAll())
     .subscribe(subscription('mergeAll'));
 
@@ -24,6 +23,22 @@ range(1, 3)
             .pipe(take(x), map(q => i))
     }))
     .subscribe(subscription('concatMap'))
+
+const z1 = of('Hi');
+const z2 = of('Igor');
+const z3 = of('!').pipe(delay(15000));
+
+zip(z1, z2, z3)
+    .subscribe(subscription('zip'));
+
+const t1 = timer(1000, 2000);
+const t2 = timer(2000, 2000);
+const t3 = timer(3000, 2000);
+const t4 = timer(4000, 2000);
+    
+combineLatest(t1, t2, t3, t4)
+    .pipe(take(10))
+    .subscribe(subscription('combineLatest'));
 
 function subscription(name) {
     return {
